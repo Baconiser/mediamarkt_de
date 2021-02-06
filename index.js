4d1a3cdd202830bc5e4a47901557546f2209563c
michael.lebedew <michael.lebedew@web-dienstleister.de>
2021-02-07 00:28:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer-extra");
+const cheerio = require("cheerio");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth"),
+    http = require("http"),
+    fs = require("fs"),
+    url = require("url");
+
+
+puppeteer.use(StealthPlugin());
+
+
+async function getState() {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://www.mediamarkt.de/de/product/_microsoft-xbox-series-x-1-tb-2677360.html");
+
+        const html = await page.content();
+        const $ = cheerio.load(html);
+        //let availableDate = $("[data-test=\"mms-delivery-online-availability\"] span[font-size=\"sm\"]");
+        const state = $("[data-test=\"pdp-product-not-available\"]").text() || $("[data-test=\"mms-delivery-online-availability\"]").text();
+        await browser.close();
+        return state;
+    } catch (error) {
+        return error;
+    }
+}
+
+http.createServer(async (req, res) => {
+    const state = await getState();
+    const html = `<html lang="de">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    </head>
+    <body>${state}</body>
+    </html>;`;
+    res.writeHead(200, { "Content-Length": html.length });
+    res.write(html);
+    res.end();
+
+}).listen(8800);
