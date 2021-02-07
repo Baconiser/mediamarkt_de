ab2571bdc02761d9c2c765d30eaad8db382dc797
michael.lebedew <michael.lebedew@web-dienstleister.de>
2021-02-07 01:00:32 +0100

    Initial commit

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ puppeteer.use(StealthPlugin());
 async function getState() {
     try {
         const browser = await puppeteer.launch({
-            headless: true, args: [ "--no-sandbox",
-                "--disable-setuid-sandbox" ]
+            headless: true, args:
+                [ "--no-sandbox",
+                    "--disable-setuid-sandbox" ]
         });
         const page = await browser.newPage();
         await page.goto("https://www.mediamarkt.de/de/product/_microsoft-xbox-series-x-1-tb-2677360.html");
